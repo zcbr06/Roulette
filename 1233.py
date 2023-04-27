5d7405025a41a63baaee9836d54593dd51a23db6
zcbr06 <zcbr06@gmail.com>
2023-04-27 21:29:11 +0300

    Добавил Уведомление о новом барабане

--- 1233.py
+++ 1233.py
@@ -22,7 +22,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 # Функция для обработки команды /variable
 async def get_variable(update, context):
     global a 
-    if a == []: a = ["Выстрел!", "Клик", "Клик", "Клик", "Клик", "Клик"]
+    if a == []: 
+        await update.message.reply_text("Новый барабан!")
+        a = ["Выстрел!", "Клик", "Клик", "Клик", "Клик", "Клик"]
     var = random.choice(a)  # выбираем случайную переменную из списка  
     a.remove(var)  # удаляем выбранную переменную из списка
     await context.bot.send_message(chat_id=update.effective_chat.id, text=var)  # отправляем сообщение с выбранной переменной
