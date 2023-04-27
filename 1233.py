914d4d234b8ceb512096d89c02ae49daa215990b
zcbr06 <zcbr06@gmail.com>
2023-04-27 21:12:04 +0300

    Ну теперь точно все

--- 1233.py
+++ 1233.py
@@ -16,13 +16,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
     await update.message.reply_html(
-        rf"Привет! {user.mention_html()}, используй (/variable)",
+        rf"Привет! {user.mention_html()}",
     )
     
 # Функция для обработки команды /variable
 async def get_variable(update, context):
     global a 
-    if a == []: a = b
+    if a == []: a = ["Выстрел!", "Клик", "Клик", "Клик", "Клик", "Клик"]
     var = random.choice(a)  # выбираем случайную переменную из списка  
     a.remove(var)  # удаляем выбранную переменную из списка
     await context.bot.send_message(chat_id=update.effective_chat.id, text=var)  # отправляем сообщение с выбранной переменной
